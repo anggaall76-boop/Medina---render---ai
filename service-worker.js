cfa63bb6cb88125c93707ae1b43247f3a8bec7ba
anggaall76-boop <angga.all76@gmail.com>
2026-08-09 09:57:04 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,101 @@
+const CACHE_NAME = "medina-render-ai-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+
+self.addEventListener(
+    "install",
+    function(event){
+
+        event.waitUntil(
+
+            caches.open(
+                CACHE_NAME
+            ).then(
+
+                function(cache){
+
+                    return cache.addAll(
+                        FILES_TO_CACHE
+                    );
+
+                }
+
+            )
+
+        );
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    function(event){
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            ).then(
+
+                function(response){
+
+                    return response ||
+                    fetch(event.request);
+
+                }
+
+            )
+
+        );
+
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    function(event){
+
+        event.waitUntil(
+
+            caches.keys().then(
+
+                function(cacheNames){
+
+                    return Promise.all(
+
+                        cacheNames.map(
+
+                            function(cacheName){
+
+                                if(
+                                    cacheName !==
+                                    CACHE_NAME
+                                ){
+
+                                    return caches.delete(
+                                        cacheName
+                                    );
+
+                                }
+
+                            }
+
+                        )
+
+                    );
+
+                }
+
+            )
+
+        );
+
+    }
+);
